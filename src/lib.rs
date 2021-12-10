409f72b7c26e20d7a1114b5e6a661f2867052132
tangentstorm <michal.wallace@gmail.com>
2021-12-10 07:31:01 -0500

    rename native class to JLang

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 use gdnative::prelude::*;
 
-#[derive(NativeClass)]#[inherit(Node)]
-pub struct HelloWorld;
-
-impl HelloWorld {
-  fn new(_owner: &Node) -> Self { HelloWorld }}
+#[derive(NativeClass)] #[inherit(Node)]
+pub struct JLang;
 
 #[methods]
-impl HelloWorld {
-  #[export] fn _ready(&self, _owner: &Node) {
-    godot_print!("Hello, world!"); }}
+impl JLang {
 
-// -- init godot --
-fn init(handle: InitHandle) { handle.add_class::<HelloWorld>(); }
+  fn new(_owner: &Node) -> Self { JLang }
+
+  #[export] fn _ready(&self, _owner: &Node) {
+    godot_print!("Hello from jlang-rs-gd!"); }}
+
+fn init(handle: InitHandle) {
+  handle.add_class::<JLang>(); }
+
 godot_init!(init);
