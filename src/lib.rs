d894c711553ed1fb2625f3a8c584e0a0980cf0ac
tangentstorm <michal.wallace@gmail.com>
2023-01-13 16:55:42 -0500

    update to godot-rust 0.11 and godot 3.5.1

--- src/lib.rs
+++ src/lib.rs
@@ -16,19 +16,19 @@ impl JLang {
 
   /// run a j command and return a string variant
   #[export] fn cmd_s(&self, _:&Node, s:String)->Variant {
-    Variant::from_str(self.jp.cmd_s(&s).as_str()) }
+    Variant::new(self.jp.cmd_s(&s).as_str()) }
 
   /// fetch a j variable by name (nouns only)
   #[export] fn getv(&self, _:&Node, v:String)->Variant {
     let jv = self.jp.get_v(&v);
     return match jv.data {
-      JData::Int(i) => Variant::from_i64(i),
+      JData::Int(i) => Variant::new(i),
       JData::IntV(v) => {
-        if jv.rank > 1 { Variant::from_str("TODO: rank>1") }
+        if jv.rank > 1 { Variant::new("TODO: rank>1") }
         else {
           let vi32:Vec<i32> = v.iter().map(|&x| x as i32).collect();
-          TypedArray::from_vec(vi32).to_variant() }},
-      _ => Variant::new() }}
+          PoolArray::from_vec(vi32).to_variant() }},
+      _ => Variant::new(0) }}
 
   /// run a j command and return the actual data as a variant.
   #[export] fn cmd(&self, n:&Node, s:String)->Variant {
