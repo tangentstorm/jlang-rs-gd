527e342cf75408acb8c6426c5233a1507bfdf220
tangentstorm <michal.wallace@gmail.com>
2023-01-14 15:09:40 -0500

    demo: update gdscript to godot4

--- demo/JConsole.gd
+++ demo/JConsole.gd
@@ -1,13 +1,16 @@
 extends Control
 
-onready var JI = $"../jlang-rs-gd" # J interpreter
-onready var output = $output
+@onready var JI = $"../jlang-rs-gd" # J interpreter
+@onready var output:RichTextLabel = $output
+@onready var input:LineEdit = $input
 
 func _ready():
 	print("J_HOME:", OS.get_environment('J_HOME'))
 	output.scroll_following = true
 
-func _on_input_text_entered(s):
+
+func _on_input_text_submitted(s):
 	var res = JI.cmd_s(s)
-	output.bbcode_text += '[color=#77ff33]   '+s+'[/color]\n'
-	output.bbcode_text += res + '\n'
+	output.text += '[color=#77ff33]   '+s+'[/color]\n'
+	output.text += res + '\n'
+	input.clear()
